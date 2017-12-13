454fb38077e1d01069821f41576bff9f578c9aeb
Sergey Solovev <solovev@ws-finance.com>
2017-12-13 11:26:02 +0300

    fix playbooks

--- site/maptaxonomy/maptaxonomy/settings.py
+++ site/maptaxonomy/maptaxonomy/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '&i(q5j-cew526w5aa+%5*7nbt@gwtkyc*jydq2g%zw&pfiu)2t'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [ u'34.212.26.94', 'nina1603.github.io', 'maptaxonomy.ru', 'www.maptaxonomy.ru' ]
+ALLOWED_HOSTS = [ u'34.212.26.94', 'nina1603.github.io', 'maptaxonomy.ru', 'www.maptaxonomy.ru', '104.154.77.126' ]
 
 
 # Application definition
